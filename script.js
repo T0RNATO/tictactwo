2165f03a9d5053a96cc0787dd61d3ffa0039edd9
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-30 14:05:36 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var current = "o";
+var i = 0;
+function clicked(me) {
+  if(!me.innerText) {
+    me.innerText = current;
+    if (current == "o") {
+      current = "x";
+      document.getElementById("t").innerText = "Current turn: X"
+    }
+    else {
+      current = "o";
+      document.getElementById("t").innerText = "Current turn: O"
+    }
+  }
+}
+function resetall() {
+  for (var i = 0; i < 64; i++) {
+    document.getElementsByTagName("div")[i].innerText = "";
+  }
+}
